197921449a586d19f63ac59300ab8041af8fe528
Guilherme Lopes <guilhermxlopes@gmail.com>
2022-11-23 23:46:18 -0300

    Update with componentes and security

--- src/main/java/com/api/parkingcontrolapi/configs/security/WebSecurityConfigV2.java
+++ src/main/java/com/api/parkingcontrolapi/configs/security/WebSecurityConfigV2.java
@@ -3,38 +3,29 @@ package com.api.parkingcontrolapi.configs.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
-//@Configuration
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+@Configuration
+public class WebSecurityConfigV2 {
 
-    final UserDetailsServiceImpl userDetailsService;
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-    public WebSecurityConfig(UserDetailsServiceImpl userDetailsService) {
-        this.userDetailsService = userDetailsService;
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic().and().authorizeRequests()
+        http
+                .httpBasic().and().authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/parking-spot/**").permitAll()
                 .antMatchers(HttpMethod.POST, "/parking-spot").hasRole("USER")
                 .antMatchers(HttpMethod.DELETE, "/parking-spot/**").hasRole("ADMIN")
                 .anyRequest().authenticated().and().csrf().disable();
-    }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+        return http.build();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
 }
